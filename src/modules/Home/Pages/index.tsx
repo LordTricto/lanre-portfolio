a1279013e9a26e74b50c8e6fe8470d190377b0e2
Olamide Idowu <lammy.idowu@gmail.com>
2022-11-11 16:12:46 +0100

    Update index.tsx

--- src/modules/Home/Pages/index.tsx
+++ src/modules/Home/Pages/index.tsx
@@ -246,16 +246,19 @@ function Home(): JSX.Element {
 				</div>
 				<div className="w-full">
 					<div className="gsap-imgs-1 opacity-0 relative h-108 xs:!h-[560px] sm:!h-[600px] md:!h-[820px] lg:!h-[1000px] xl:!h-[1220px] w-full max-w-[1720px] bg-purple-100 ">
-						<img
+						<div
 							className={
-								`gsap-img-1 absolute top-8 3xs:top-16 w-max object-contain ` +
-								`grayscale transition !ease-linear duration-300 hover:grayscale-0 ` +
+								`gsap-img-1 absolute top-8 3xs:top-16 ` +
 								`h-[100px] 3xs:h-[150px] xs:h-[220px] md:!h-[320px] lg:!h-[520px] xl:!h-[720px] ` +
 								`-left-[83px] 3xs:-left-[125px] xs:-left-[185px] md:!-left-[270px] lg:!-left-[440px] xl:!-left-[610px] `
 							}
-							src={ImageOne}
-							alt="chair"
-						/>
+						>
+							<img
+								className="object-contain h-full w-max grayscale transition !ease-linear duration-300 hover:grayscale-0 mx-auto "
+								src={ImageOne}
+								alt="chair"
+							/>
+						</div>
 						<div
 							className={
 								`absolute top-0 left-0 h-full w-full ` +
@@ -264,25 +267,25 @@ function Home(): JSX.Element {
 						>
 							<div className="relative w-full h-full">
 								<img
-									className={
-										"object-contain h-full w-max grayscale transition !ease-linear duration-300 hover:grayscale-0 mx-auto pl- " +
-										`h-[150px] 3xs:h-[200px] xs:h-[300px] md:!h-[400px] lg:!h-[620px] xl:!h-[820px]`
-									}
+									className="object-contain h-full w-max grayscale transition !ease-linear duration-300 hover:grayscale-0 mx-auto "
 									src={ImageTwo}
 									alt="map"
 								/>
 							</div>
 						</div>
-						<img
+						<div
 							className={
-								`gsap-img-3 absolute top-4 3xs:top-8 w-max object-contain ` +
-								`grayscale transition !ease-linear duration-300 hover:grayscale-0 ` +
+								`gsap-img-3 absolute top-4 3xs:top-8 ` +
 								`h-[150px] 3xs:h-[200px] xs:h-[300px] md:!h-[400px] lg:!h-[620px] xl:!h-[820px] ` +
 								`-right-[63px] 3xs:-right-[82px] xs:-right-[125px] md:!-right-[168px] lg:!-right-[260px] xl:!-right-[345px] `
 							}
-							src={ImageThree}
-							alt="people"
-						/>
+						>
+							<img
+								className="object-contain h-full w-max grayscale transition !ease-linear duration-300 hover:grayscale-0 mx-auto "
+								src={ImageThree}
+								alt="people"
+							/>
+						</div>
 					</div>
 					<div className="relative [@media(min-width:1400px)]:!h-[720px] [@media(min-width:1240px)]:!h-[620px] [@media(min-width:1100px)]:!h-[560px] [@media(min-width:1024px)]:!h-[520px] [@media(min-width:880px)]:h-108 [@media(min-width:775px)]:h-96 sm:h-80 xs:h-60 4xs:h-44 h-36">
 						<img
